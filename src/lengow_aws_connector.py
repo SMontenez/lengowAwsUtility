34851c242c3d6ef2c840773b3a184a58f25e3c40
SMontenez <sebastien.montenez@gmail.com>
2016-06-15 18:02:25 +0200

    Add method to create the fulfill order on AWS api
    This method has to be called with an order extracted from Lengow api. It
    will then call the CreateFulfillmentOrder on AWS with parameters
    extracted from this order's data.

--- src/lengow_aws_connector.py
+++ src/lengow_aws_connector.py
@@ -22,6 +22,11 @@ AWS_APP_ID = os.environ.get("AWS_APP_ID")
 AWS_APP_SECRET = os.environ.get("AWS_APP_SECRET")
 AWS_BUCKET = os.environ.get("AWS_BUCKET")
 
+not_allowed_marketplaces = ["amazon"]
+
+order_comment = """ Merci pour votre commande. Pour les retours, veuillez prendre contact avec nous
+                    et retourner le produit dans son emballage d'origine"""
+
 
 def get_orders_from_lengow(
         start_date, end_date, account_id=ACCOUNT_ID, group_id=GROUP_ID, flux_id=FLUX_ID,
@@ -85,4 +90,89 @@ def preview_aws_shipment(order):
 
     data = dict(Action="GetFulfillmentPreview")
     data.update(enumerate_data(aws_order_data))
+    return mws_shipments.make_request(data, "POST")
+
+
+def create_aws_order(lengow_order):
+    """
+    Process the data returned by Lengow api, to adapat it for the AWS CreateFulfillmentOrder.
+    """
+    if (
+            lengow_order["marketplace"] in not_allowed_marketplaces or
+            lengow_order["order_status"]["lengow"] != "processing"):
+        return
+
+    # Data that will be sent to AWS CreateFulfillmentOrder method
+    aws_order_data = {}
+
+    # Unique ID for this order
+    order_id = "{marketplace}_{order_id}".format(
+        marketplace=lengow_order["marketplace"], order_id=lengow_order["order_id"])
+    aws_order_data["SellerFulfillmentOrderId"] = order_id
+    aws_order_data["DisplayableOrderId"] = order_id
+
+    # The date of the fulfillment order. Displays as the order date in customer-facing materials
+    # such as the outbound shipment packing slip.
+    aws_order_data["DisplayableOrderDateTime"] = "{date}T{time}".format(
+            date=lengow_order["order_purchase_date"], time=lengow_order["order_purchase_heure"])
+
+    # Order-specific text that appears in customer-facing materials such as the outbound shipment
+    # packing slip.
+    aws_order_data["DisplayableOrderComment"] = order_comment
+
+    aws_order_data["ShippingSpeedCategory"] = (
+            "Expedited" if float(lengow_order["order_shipping"]) > 0 else "Standard")
+
+    # The destination address for the fulfillment order.
+    address = lengow_order["delivery_address"]
+    country_iso = address["delivery_country_iso"]
+    aws_order_data["DestinationAddress"] = {
+            "Name": "{firstname} {lastname}".format(
+                firstname=address["delivery_firstname"].title(),
+                lastname=address["delivery_lastname"]).title(),
+            "Line1": address["delivery_address"].title(),
+            "Line2": address["delivery_address_2"].title(),
+            "Line3": address["delivery_address_complement"].title(),
+            "City": address["delivery_city"].title(),
+            "StateOrProvinceCode": address["delivery_zipcode"],
+            "PostalCode": address["delivery_zipcode"],
+            "CountryCode": country_iso,
+            "PhoneNumber": address["delivery_phone_mobile"]
+    }
+    # As specified in AWS documentation, don"t include city if country is JP.
+    if country_iso == "JP":
+        aws_order_data["DestinationAddress"].pop("City")
+
+    aws_order_data["Items"] = []
+    for product in lengow_order["cart"]["products"]:
+        item = {
+                "SellerSKU": product["sku"],
+                "SellerFulfillmentOrderItemId": product["sku"],
+                "Quantity": product["quantity"],
+                "PerUnitDeclaredValue": {
+                    "Value": product["price_unit"], "CurrencyCode": lengow_order["order_currency"]
+                }
+        }
+        aws_order_data["Items"].append(item)
+
+    # aws_order_data["NotificationEmailList"] = [address["delivery_email"]]
+
+    mws_shipments = mws.OutboundShipments(
+        access_key=MWS_ACCESS_KEY, secret_key=MWS_SECRET_KEY,
+        account_id=MWS_MERCHANT_ID, region="FR")
+
+    data = dict(Action="CreateFulfillmentOrder")
+    data.update(enumerate_data(aws_order_data))
+    return mws_shipments.make_request(data, "POST")
+
+
+def cancel_aws_order(order_id):
+    """
+    Cancel a previously made order with its id.
+    """
+    mws_shipments = mws.OutboundShipments(
+        access_key=MWS_ACCESS_KEY, secret_key=MWS_SECRET_KEY,
+        account_id=MWS_MERCHANT_ID, region="FR")
+
+    data = dict(Action="CancelFulfillmentOrder", SellerFulfillmentOrderId=order_id)
     return mws_shipments.make_request(data, "POST")
--- tests/test_lengow_aws_connector.py
+++ tests/test_lengow_aws_connector.py
@@ -1,23 +1,89 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
+import random
+import time
 import unittest
 
-import lengow_aws_connector
+from mws import mws
+
+from src import lengow_aws_connector
 
 
-class LengowAwsConnector(unittest.TestCase):
+class LengowAwsConnectorTestCase(unittest.TestCase):
+
+    def setUp(self, *args, **kwargs):
+        super(LengowAwsConnectorTestCase, self).setUp(*args, **kwargs)
+        self.MWS_ACCESS_KEY = lengow_aws_connector.MWS_ACCESS_KEY
+        self.MWS_SECRET_KEY = lengow_aws_connector.MWS_SECRET_KEY
+        self.MWS_MERCHANT_ID = lengow_aws_connector.MWS_MERCHANT_ID
+        self.orders_data = lengow_aws_connector.get_orders_from_lengow(
+            date.today() - timedelta(days=1), date.today())
 
     def test_get_orders_from_lengow(self):
         """
         get_orders_from_lengow() should return orders in a dict.
         """
-        start_date = date.today() - timedelta(days=1)
-        end_date = date.today()
-
-        orders_data = lengow_aws_connector.get_orders_from_lengow(start_date, end_date)
-
         # Data should be returned as a dict
-        self.assertTrue(isinstance(orders_data, dict))
-        self.assertEqual(set(orders_data.keys()), set(['orders', 'orders_count']))
+        self.assertTrue(isinstance(self.orders_data, dict))
+        self.assertEqual(set(self.orders_data.keys()), set(["orders", "orders_count"]))
+
+    def test_create_aws_order(self):
+        """
+        Validate that the method correctly creates the order via the amazon API. We directly cancel
+        this order as we just want to test that it works.
+        """
+        # Get a valid SKU to test ie one that is in inventory
+        inv = mws.Inventory(
+            access_key=self.MWS_ACCESS_KEY, secret_key=self.MWS_SECRET_KEY,
+            account_id=self.MWS_MERCHANT_ID, region="FR")
+        inv_supply = inv.list_inventory_supply(
+            datetime=(datetime.now()-timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+        self.assertEqual(inv_supply.response.status_code, 200)
+
+        # A random SKU of an article we have in our inventory
+        valid_sku = random.choice(
+            inv_supply.parsed["InventorySupplyList"]["member"])["SellerSKU"]["value"]
+
+        # Orders that will pass all tests to be created by our method
+        valid_orders = [
+            o for o in self.orders_data["orders"] if
+            o["marketplace"] not in lengow_aws_connector.not_allowed_marketplaces and
+            o["order_status"]["lengow"] == "processing"]
+
+        # Get one of these orders
+        test_order = valid_orders[0]
+        # Use a fake unique order id
+        test_order["order_id"] = "{:{df}}".format(datetime.now(), df="%Y%m%dT%H:%M:%S")
+        # Modify its SKU so that AWS doesn't reject our request
+        test_order["cart"]["products"] = [test_order["cart"]["products"][0]]
+        test_order["cart"]["products"][0]["sku"] = valid_sku
+
+        # Max 40 chars
+        test_order_id = "{}_{}".format(test_order["marketplace"], test_order["order_id"])[:40]
+
+        # Save order_id so we can manually handle it if test breaks
+        with open("tests/data/test_order_ids.json", "a+") as file:
+            file.write("{}\n".format(test_order_id))
+
+        # Try to create an order via our method
+        try:
+            req = lengow_aws_connector.create_aws_order(test_order, test_order_id)
+        except mws.MWSError as e:
+            print(e.response.text)
+        self.assertEqual(req.response.status_code, 200)
+
+        # Wait a little so first request is processed
+        time.sleep(5)
+        # Cancel the order as we don't want it to be really activated
+        rep = 0
+        while rep != 200:
+            try:
+                req = lengow_aws_connector.cancel_aws_order(test_order_id)
+            except mws.MWSError as e:
+                print(e.response.text)
+                break
+            rep = req.response.status_code
+
 
 if __name__ == "__main__":
     unittest.main()
